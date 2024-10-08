166f5b24d90452761bf9e7d91fcc632d8a8fb791
shreyas s <shreyas.s.pillai@gmail.com>
2024-10-08 14:05:23 +0530

    tdl all completed

--- Javascript/Day6_Js/tdl/tdl.js
+++ Javascript/Day6_Js/tdl/tdl.js
@@ -3,32 +3,56 @@ document.addEventListener("DOMContentLoaded", function() {
     const taskList = document.querySelector('.task-list');
 
     addTaskBtn.addEventListener('click', addTask);
+
+    taskList.addEventListener("click",(event)=>{
+        
+        if (event.target.classList.contains("delete-btn")){
+            const taskDiv = event.target.closest(".task");
+            taskList.removeChild(taskDiv);
+        }
+        else if( event.target.type ==="checkbox"){
+
+            const taskTextSpan = event.target.nextElementSibling;
+            if(event.target.checked){
+                taskTextSpan.style.textDecoration ="line-through";
+            }
+            else{
+                taskTextSpan.style.textDecoration ="none";
+            }
+        }
+    });
+
+
 });
 
 function addTask() {
-    const taskInput = document.getElementById('task-input');
+
+    const taskInput = document.getElementById("task-input");
     const taskText = taskInput.value;
 
-    if (taskText === '') return;
+    if(taskText === "")
+        return;
 
-    const taskList = document.querySelector('.task-list');
+    const taskList = document.querySelector(".task-list");
     const taskDiv = document.createElement('div');
-    taskDiv.classList.add('task');
+    taskDiv.classList.add("task");
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
 
     const taskTextSpan = document.createElement('span');
     taskTextSpan.textContent = taskText;
 
     const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.textContent = String.fromCharCode("&#128465;");
 
-    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(checkBox);
     taskDiv.appendChild(taskTextSpan);
     taskDiv.appendChild(deleteBtn);
+
     taskList.appendChild(taskDiv);
 
-    taskInput.value = '';
+    
+
 }
